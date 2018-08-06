ffb6078f5f56653256b454110a84b5ebc293560e
sumitkumar1207 <logsumit28@gmail.com>
2018-08-06 18:33:19 +0530

    'intial'

---
+++ src/components/Form.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+  render() {
+    return (
+      <form onSubmit={this.props.getWeather}>
+        <input type="text" id="city" name="city" placeholder="Enter City..." />
+        <input type="text" name="country" placeholder="Enter Country..." />
+        <button>Get Weather</button>
+      </form>
+    )
+  }
+}
+
+export default Form;
