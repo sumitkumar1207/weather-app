83435978fb0554cec9aadc0ee6253a13e84a0ce2
sumitkumar1207 <logsumit28@gmail.com>
2018-08-06 19:11:34 +0530

    'Second'

--- src/App.js
+++ src/App.js
@@ -40,6 +40,8 @@ class App extends Component {
         error: "Please Enter the values"
       });
     }
+    document.getElementById("create-weather-form").reset();
+
   }
   render() {
     return (
@@ -52,7 +54,7 @@ class App extends Component {
                 <Titles />
               </div>
               <div className="col-xs-7 form-container">
-                <Form getWeather={this.getWeather} />
+                <Form myFormRef={this.myFormRef} getWeather={this.getWeather} />
                 <Weather
                 temperature={this.state.temperature}
                 city={this.state.city}
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Form extends Component {
   render() {
     return (
-      <form onSubmit={this.props.getWeather}>
+      <form id="create-weather-form" onSubmit={this.props.getWeather} >
         <input type="text" id="city" name="city" placeholder="Enter City..." />
         <input type="text" name="country" placeholder="Enter Country..." />
         <button>Get Weather</button>
